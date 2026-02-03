3f919009068497845ccb0d678197f14d6c8f5442
masteriot310 <masteriot75@gmail.com>
2026-02-03 15:38:22 +0800

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from extract import fetch_passenger_counts_df
-
-df = fetch_passenger_counts_df()
-#print(df.info())
-# print(df.describe())
-# print(df.head())
-# print(df.tail())
-print(df.to_string())
-
